0cdcaee4caa0ae1d5d04ae063487629c716f22e4
Chidambaran S <chiddu@gmail.com>
2013-03-28 14:51:34 +0530

    Saving my work now

--- WEB-INF/classes/servlet/AddField.java
+++ WEB-INF/classes/servlet/AddField.java
@@ -11,7 +11,16 @@ import org.json.*;
 
 public class AddField extends HttpServlet 
 {
+
+
+		public writeResponse(HttpServletResponse response,
+			JSONObject obj)
+			{
+        PrintWriter out = response.getWriter();
+				out.print(obj.toString());
+			}
 		/* we start with just the list of all the periods */
+
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws IOException, ServletException
     {
@@ -19,15 +28,42 @@ public class AddField extends HttpServlet
 			{
         response.setContentType("text/javascript");
 				Container cns = Container.getContainer();
-				CassStorage rms = cns.getStorage();
 
-				String name = request.getParameter("name");
-				String type = request.getParameter("type");
-				rms.saveColumn("input_field", name, "type" , type);
-        PrintWriter out = response.getWriter();
+				String action = request.getParameter("action");
+
+				/* The out object, we are all doing json here */
+				JSONObject obj = new JSONObject();
+
+				if(!action)
+				{
+					obj.put("message" , "There seems to be an empty response fired ");
+					writeResponse(response, obj);
+				}
+
+				BaseCass rms = cns.getStorage();
+
+				if(action.equalsIgnoreCase("addfield"))
+				{
+					addfield(request,response, obj);
+				}
+				else if(action.equalsIgnoreCase("addpage"))
+				{
+					addpage(request,response, obj);
+				}
+				else if(action.equalsIgnoreCase("listfields"))
+				{
+					listfields(request,response, obj);
+				}
+				else if(action.equalsIgnoreCase("addtopage"))
+				{
+					addtopage(request,response, obj);
+				}
+				else if(action.equalsIgnoreCase("addcondition"))
+				{
+					addcondition(request,response, obj);
+				}
 
 				JSONObject obj = new JSONObject();
-				obj.put("type", type);
 				obj.put("name", name);
 			}
 			catch(Exception ex)
@@ -41,4 +77,88 @@ public class AddField extends HttpServlet
     {
         doGet(request, response);
     }
+
+				public Set<String> getFieldList(BaseCass rms)
+				{
+					String instr = rms.getColData("misc", "fields","list");
+					Set<String> = MtxUtil.convertJsonToSet(instr);
+					return instr;
+				}
+				public Set<String> getPageList(BaseCass rms)
+				{
+					String instr = rms.getColData("misc", "pages","list");
+					Set<String> = MtxUtil.convertJsonToSet(instr);
+					return instr;
+				}
+
+				public void addfield((BaseCass rms, HttpServletRequest request, HttpServletResponse response, JSONObject obj)
+				{
+					Set<String> inf = getFieldList(rns);
+					String name = request.getParameter("name");
+
+					if(inf.contains(name))
+					{
+						obj.put("message", name + " is already a field, please choose another name ");
+						writeReponse(response, obj);
+						return;
+					}
+
+					String type = request.getParameter("type");
+					rms.saveColumn("input_field", name, "type" , type);
+
+					String values = request.getParameter("values");
+					if(values != null)
+					{
+						rms.saveColumn("input_field", name, "values" , values);
+					}
+
+					inf.add(name);
+					JSONArray jso = new JSONArray(inf);
+					rms.saveColumn("misc", "fields", "list" , jso.toString())
+
+					obj.put("message", "Added field successfully");
+//					getfields(request,response, obj);
+					return;
+				}
+
+				public void addpage((HttpServletRequest request, HttpServletResponse response, JSONObject obj)
+				{
+					Set<String> inf = getPageList(rns);
+					String title = request.getParameter("title");
+
+					if(inf.contains(title))
+					{
+						obj.put("message", title + " is already a field, please choose another title ");
+						writeReponse(response, obj);
+						return;
+					}
+					inf.add(title);
+					JSONArray jso = new JSONArray(inf);
+					rms.saveColumn("misc", "pages", "list" , jso.toString())
+					obj.put("message", "Added page successfully");
+					return;
+				}
+
+				public void listfields((HttpServletRequest request, HttpServletResponse response, JSONObject obj)
+				{
+					Set<String> inf = getFieldList(rns);
+					obj.put("fields", inf);
+					for(String eachfield : inf)
+					{
+						HashMap<String,String> samo = cms.		
+					}
+					String title = request.getParameter("title");
+
+					if(inf.contains(title))
+					{
+						obj.put("message", title + " is already a field, please choose another title ");
+						writeReponse(response, obj);
+						return;
+					}
+					inf.add(title);
+					JSONArray jso = new JSONArray(inf);
+					rms.saveColumn("misc", "pages", "list" , jso.toString())
+					obj.put("message", "Added page successfully");
+					return;
+				}
 }
---
+++ WEB-INF/classes/servlet/TimePass.java
@@ -0,0 +1,38 @@
+package servlet;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+import proto.*;
+import org.json.*;
+
+public class TimePass extends HttpServlet 
+{
+		/* we start with just the list of all the periods */
+		private int dil = 0;
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+			try
+			{
+        response.setContentType("text/javascript");
+				dil = dil + 1;
+
+        PrintWriter out = response.getWriter();
+				out.print(dil);
+			}
+			catch(Exception ex)
+			{
+				ex.printStackTrace();
+			}
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        doGet(request, response);
+    }
+}
