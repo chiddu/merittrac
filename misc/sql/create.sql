c061fedff3846a68d8a509baac73964b7fe9096b
S Chidambaran <chiddu@metrixline.com>
2013-05-07 19:37:47 +0530

    just the commit.sql

---
+++ misc/sql/create.sql
@@ -0,0 +1,25 @@
+
+input_field
+	id, 
+	name, 
+	type, 
+	value (value would be a blob) 
+
+pages
+	list -> full definition (blob)
+	id -> page (order)
+	title  - page title
+	style_id
+
+
+condition :
+  id 
+	input_field_id
+	input_field_values (blob) - json define this
+
+condition_output
+1) Output field
+2) Output value(s)
+3) Auto
+
+Conditional field in page.
